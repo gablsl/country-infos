900d1895557fbab4c93570073285a0287084471a
gablsl <gabrielsousadeveloper@gmail.com>
2024-07-28 14:26:03 -0300

    feat: adjustment to global layout

--- src/layouts/DefaultLayout.tsx
+++ src/layouts/DefaultLayout.tsx
@@ -1,12 +1,15 @@
 import { Layout } from 'antd';
 import { Outlet } from 'react-router-dom';
 import { Header } from '../components/Header';
+import { Content } from 'antd/es/layout/layout';
 
 export function DefaultLayout() {
   return (
     <Layout>
       <Header />
-      <Outlet />
+      <Content style={{ padding: '0 50px' }}>
+        <Outlet />
+      </Content>
     </Layout>
   );
 }
