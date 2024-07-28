0dce90dd4c32178ec67605e08595bbd32c164c39
gablsl <gabrielsousadeveloper@gmail.com>
2024-07-28 19:20:38 -0300

    feat: adding countries list component in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,12 @@
+import { Space } from 'antd';
 import { Searches } from '../../components/Searches';
+import { CountriesList } from '../../components/CountriesList';
 
 export function HomePage() {
   return (
-    <>
+    <Space direction='vertical' size={48}>
       <Searches />
-    </>
+      <CountriesList />
+    </Space>
   );
 }
