fd35fbf42aae62c8d0d55b4505a969888705e57b
gablsl <gabrielsousadeveloper@gmail.com>
2024-07-29 13:11:33 -0300

    feat: adding country page to router

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { DefaultLayout } from './layouts/DefaultLayout';
 import { HomePage } from './pages/Home';
+import { CountryPage } from './pages/Country';
 
 export function Router() {
   return (
     <Routes>
       <Route path='/' element={<DefaultLayout />}>
         <Route path='/' element={<HomePage />} />
+        <Route path='/country/:name' element={<CountryPage />} />
       </Route>
     </Routes>
   );
