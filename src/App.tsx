57930cde6e758b535252c7c1e0608f130bf859e2
gablsl <gabrielsousadeveloper@gmail.com>
2024-07-29 10:53:34 -0300

    feat: adding react query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import { BrowserRouter } from 'react-router-dom';
 import { Router } from './Routes';
 import { ConfigProvider } from 'antd';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
+  const queryClient = new QueryClient();
+
   return (
     <ConfigProvider>
-      <BrowserRouter>
-        <Router />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <Router />
+        </BrowserRouter>
+      </QueryClientProvider>
     </ConfigProvider>
   );
 }
