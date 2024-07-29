950e9b6cfb5f956cb1bd7c7571c064aea1c291de
gablsl <gabrielsousadeveloper@gmail.com>
2024-07-29 10:54:36 -0300

    feat: showing fetch data to countries list

--- src/components/CountriesList/index.tsx
+++ src/components/CountriesList/index.tsx
@@ -1,34 +1,25 @@
 import { Col, Divider, Pagination, Row } from 'antd';
+import { useCountriesData } from '../../utils/useCountriesData';
 import { Country } from './Country';
 
 export function CountriesList() {
+  const { data } = useCountriesData();
+
   return (
     <Divider>
       <Row>
-        <Col span={6}>
-          <Country />
-        </Col>
-        <Col span={6}>
-          <Country />
-        </Col>
-        <Col span={6}>
-          <Country />
-        </Col>
-        <Col span={6}>
-          <Country />
-        </Col>
-        <Col span={6}>
-          <Country />
-        </Col>
-        <Col span={6}>
-          <Country />
-        </Col>
-        <Col span={6}>
-          <Country />
-        </Col>
-        <Col span={6}>
-          <Country />
-        </Col>
+        {data?.map((country) => (
+          <Col span={6}>
+            <Country
+              key={country.name.common}
+              name={country.name.common}
+              population={country.population}
+              region={country.region}
+              capital={country.capital}
+              flagUrl={country.flags.png}
+            />
+          </Col>
+        ))}
       </Row>
       <Pagination defaultCurrent={1} total={50} align='center' />
     </Divider>
